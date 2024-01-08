9e5a2f4f38853f68347613d2482349143dc8ff35
Gresdian <chris_da98@outlook.it>
2024-01-08 22:21:04 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+      
+}
+</script>
+<template lang="">
+  <div>
+    
+  </div>
+</template>
+<style lang="scss">
+  @use './styles/partials/variables';
+  @use './styles/generals.scss' ;
+</style>
