52ae6c043f7d8b154e3fc3bc6e59917968b5479e
Gresdian <chris_da98@outlook.it>
2024-01-09 09:55:57 +0100

    add file and img

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <script>
+import Appheader from './components/Appheader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 export default {
+  components : {
+    AppFooter,
+    AppMain,
+    Appheader
+  }
       
 }
 </script>
 <template lang="">
   <div>
-    
+    <Appheader />
+    <AppMain />
+    <AppFooter />
   </div>
 </template>
 <style lang="scss">
   @use './styles/partials/variables';
   @use './styles/generals.scss' ;
+  
 </style>
